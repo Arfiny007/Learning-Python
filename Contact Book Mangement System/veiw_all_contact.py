286bbbe77d67abf95583c426d0681b5b9878bb57
Yeasin Arfin <Arfiny007@gmail.com>
2024-12-05 17:12:57 +0600

    Delete Contact Book Mangement System directory

--- Contact Book Mangement System/veiw_all_contact.py
+++
@@ -1,8 +0,0 @@
-
-def view_all_contact(all_contact) :
-    if all_contact:
-        print("\nList of Contacts:")
-        for contact in all_contact :
-            print(f"Name : {contact['name']} || Phone Number : {contact['phone_number']} || Email : {contact['email']} || Address : {contact['address']}")
-    else :
-        print("No Contact Has Been Found")
